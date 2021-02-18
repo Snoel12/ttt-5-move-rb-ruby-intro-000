382180ff3db745035409578e35d6ca0e01bf04c4
congenial-entropy-ca7c <snoel45@gmail.com>
2021-02-18 13:23:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,11 +6,12 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-def input_to_index(string)
-  puts "#{string.to_i - 1}"
+def input_to_index(input)
+  input.to_i - 1
 end
-def move(array, index, character=x)
-  puts input_to_index("#{index}")
+def move(board, index, character=x)
+  board[index]=character
+end
 
 
 # code your input_to_index and move method here!
